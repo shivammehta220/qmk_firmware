f97dd3b4fcaa4bd0a60e3afc28d86724d12938f1
Shivam <smehta22000@gmail.com>
2023-09-02 15:31:49 -0400

    keymaps and animations

--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -7,10 +7,13 @@ BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 COMMAND_ENABLE = no         # Commands for debug and configuration
 CONSOLE_ENABLE = no         # Console for debug
 DEBOUNCE_TYPE = sym_eager_pk
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 LTO_ENABLE = yes            # Link time optimise, reduce firmware size
 MOUSEKEY_ENABLE = no        # Mouse keys
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 OLED_DRIVER = SSD1306
 OLED_ENABLE = yes
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+PROGRAMMABLE_BUTTON_ENABLE = yes
+
+WPM_ENABLE = yes
